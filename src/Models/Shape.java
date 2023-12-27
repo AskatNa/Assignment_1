caff252e2cd0e5d79143aa2d1148454d101c5a40
Аскат Наринбетов <231308@astanait.edu.kz>
2023-12-27 23:26:59 +0600

    First init

--- src/Models/Shape.java
+++ src/Models/Shape.java
@@ -5,9 +5,28 @@ public class Shape {
     private ArrayList<Point> array;
     public Shape(){
         array = new ArrayList<Point>();
-
     }
-    
+    public void addPoint(Point point){
+        array.add(point);
+    }
 
+    public double calculatePerimeter(){
+        double perimeter = 0;
 
+        for(int i = 0; i < 9; i++) perimeter += array.get(i).distanceTo(array.get(i + 1));
+        perimeter += array.get(0).distanceTo(array.get(9));
+        return perimeter;
+    }
+    public double getAverageSide(){
+        return calculatePerimeter() / 10;
+    }
+    public double getLongestSide(){
+        double maxLength = 0;
+        double current;
+        for(int i = 0; i < 9; i++){
+            current = array.get(i).distanceTo(array.get(i+1));
+            maxLength = Math.max(maxLength, current);
+        }
+        return maxLength;
+    }
 }
