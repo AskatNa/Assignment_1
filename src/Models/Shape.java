fe3357b72dac4efe040011cb31bae90363a55d77
Аскат Наринбетов <231308@astanait.edu.kz>
2023-12-27 18:34:47 +0600

    First init

---
+++ src/Models/Shape.java
@@ -0,0 +1,13 @@
+package Models;
+import java.util.ArrayList;
+public class Shape {
+
+    private ArrayList<Point> array;
+    public Shape(){
+        array = new ArrayList<Point>();
+
+    }
+    
+
+
+}
---
+++ src/MyApplication.java
@@ -0,0 +1,13 @@
+import Models.Point;
+import Models.Shape;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+public class MyApplication {
+
+
+
+}
